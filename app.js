a759589152a2d851d18e717f81dfac50643c753e
Tharunvikas55 <tharunvikasss.20msc@kongu.edu>
2024-03-15 09:26:17 +0530

    ss

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const path = require('path');
 
 const Routes=require("./routes/Routes")
 
+const Remainder = require('./model/remainder');
 
+// const accountSid = 'ACe1154afd5197349b3eae1ad34aaca4a5';
+// const authToken = 'dee16b1312b5a1badacbc9a3adf0ce52';
+// const client = require("twilio")(accountSid, authToken);
 
 //DB configuration
 const db=require("./config/keys").MongoURI;
@@ -28,10 +32,96 @@ app.use((req, res, next) => {
   next();
 });
 
+
+
+const sendMessage = async (body,id) => {
+  const accountSid = process.env.ACCOUNT_SID;
+  const authToken = process.env.AUTH_TOKEN_TWILIO;
+  const client = require("twilio")(accountSid, authToken);
+  try {
+    const message = await client.messages.create({
+      body:body,
+      from: "+12408378385",
+      to: "+919384148359",
+    });
+    
+  } catch (err) {
+    console.error(`Error sending SMS: `);
+    // Handle the error gracefully, log it, or perform other actions.
+  }
+};
+
+app.use("/admin/:id/sendRemainder", async (req, res) => {
+  const _id = req.params.id;
+  const messageBody = "Your reminder message goes here.";
+  
+    await sendMessage(messageBody,_id);
+    res.redirect("/admin/index");
+  // .catch (err) {
+  //   console.log(err);
+  //   res.status(500).send("Error sending SMS ");
+  // }
+});
+
+
 app.use('/admin',Routes);
 app.get("/",(req,res)=>{
     res.render("login",{title:"Login"});
 })
+// const sendSMS=async(body)=>{
+//   let options={
+//     from:"+12408378385",
+//     to:"9384148359",
+//     body,
+//   };
+//   try{
+//     const message=await client.messages.create(options);
+//     console.log(message);
+
+//   }
+//   catch (err){
+//     console.log(err);
+//   }
+// };
+
+// sendSMS("Next Services is approaching");
+
+
+// const sendMessage = async (body, pnum) => {
+//   const accountSid = process.env.ACCOUNT_SID;
+//   const authToken = process.env.AUTH_TOKEN_TWILIO;
+//   const client = require("twilio")(accountSid, authToken);
+
+//   try {
+//     const message = await client.messages.create({
+//       body: "bus services is approaching",
+//       from: "+12408378385",
+//       to: `+919384148359`,
+//     });
+
+//     console.log(message.sid);
+//   } catch (err) {
+//     console.error(`Error sending SMS: ${err.message}`);
+//     // Handle the error gracefully, log it, or perform other actions.
+//   }
+// };
+
+// app.get("/admin/:id/sendRemainder", async (req, res) => {
+//   const id = req.params.id;
+//   Remainder.findOne({_id:id})
+//     .then(result=>{
+//         res.render('remainder',{remainder:result,title:"Remainder Details"})
+//     })
+
+//   try {
+//     await sendMessage(req.body);
+//     res.redirect("/admin/index");
+//   } catch (err) {
+//     console.log(err);
+    
+//     res.status(500).send("Error sending SMS");
+//   }
+// });
 
 
  app.use((req,res)=>{
---
+++ controller/smsController.js
@@ -0,0 +1,5 @@
+const express=require("express");
+const Vehicle=require("../model/Vehicle");
+const Remainder = require('../model/remainder'); 
+
+require("dotenv").config();
